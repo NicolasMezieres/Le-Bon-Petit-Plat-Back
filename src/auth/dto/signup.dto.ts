3020ac65f4a0e0cb21725fad427f1bef517bc4d6
NicolasMezieres <nicolas.mezieres73@gmail.com>
2024-10-03 03:52:23 +0200

    dto auth

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,30 +1,38 @@
-import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
-export class signupDTO{
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(50)
-    firstName: string
+export class signupDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  firstName: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(50)
-    lastName: string
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  lastName: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @IsEmail()
-    @MaxLength(320)
-    email: string
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(320)
+  email: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(50)
-    username: string
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  username: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(50)
-    @MinLength(8)
-    password: string
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
+  @MaxLength(50)
+  @MinLength(8)
+  password: string;
 }
