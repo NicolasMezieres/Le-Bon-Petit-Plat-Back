ea47a5485a04ccfd8fe7ce49d224622a26185cbd
NicolasMezieres <nicolas.mezieres73@gmail.com>
2024-11-02 13:00:40 +0100

    fix deploy 8

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
