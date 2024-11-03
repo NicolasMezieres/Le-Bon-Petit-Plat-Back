547b2f79f56b950317293af2d9237984f603e692
NicolasMezieres <nicolas.mezieres73@gmail.com>
2024-11-03 21:02:58 +0100

    try vercel back

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { readFileSync } from 'fs';
-import * as https from 'https';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const httpsOptions = {
-    key: readFileSync('certificates/private.key'), // Chemin vers votre clé privée
-    cert: readFileSync('certificates/certificate.crt'), // Chemin vers votre certificat
-  };
-  https
-    .createServer(httpsOptions, app.getHttpAdapter().getInstance())
-    .listen(3001, () => {
-      console.log('HTTPS Server is running on https://localhost:3001');
-    });
   app.enableCors({
     origin: (origin, callback) => {
       console.log(`Received origin: ${origin}`);
