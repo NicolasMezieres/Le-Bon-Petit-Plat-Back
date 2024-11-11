3e6b06446683a04fc8da011cc3e25e2555194288
NicolasMezieres <nicolas.mezieres73@gmail.com>
2024-11-11 17:11:03 +0100

    remove cors

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: (origin, callback) => {
-      console.log(`Received origin: ${origin}`);
-      const allowedOrigins = [process.env.FRONT_URL];
-      console.log(process.env.FRONT_URL);
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
